6cd1b9ceea525751c0b29baf69611a12c0aa6cd1
Fake <drdolittle448@yahoo.de>
2020-12-15 20:25:55 +0100

    webpack dev-server

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,9 @@ var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
    entry: "./src/index.js",
+   devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+   },
    plugins: [
       new HtmlWebpackPlugin({
          template: "./src/index-template.html",
